6421c6f555ac664a218dfcb21998b3d6761723f6
duyle <n23dcci019@student.ptithcm.edu.vn>
2026-04-14 20:36:26 +0700

    feat: them kenh A2

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,22 +1,23 @@
-const int sensorPins[2] = {A0, A1}; 
+const int sensorPins[3] = {A0, A1, A2};
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int adcValues[2];      
-  float temperatures[2];  
+  int adcValues[3];
+  float temperatures[3];
   
-  // Vòng lặp đọc và tính toán cho từng kênh
-  for (int i = 0; i < 2; i++) {
+  for (int i = 0; i < 3; i++) {
     adcValues[i] = analogRead(sensorPins[i]);
     temperatures[i] = (adcValues[i] * 500.0) / 1023.0;
   }
   
   Serial.print(temperatures[0]);
   Serial.print(",");
-  Serial.println(temperatures[1]);
+  Serial.print(temperatures[1]);
+  Serial.print(",");
+  Serial.println(temperatures[2]);
   
   delay(1000);
 }
